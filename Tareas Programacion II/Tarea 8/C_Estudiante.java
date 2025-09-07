e8752264d3f8c405f8f7b8c39ede4cfeaeeeb410
Jeferson <jefersonmencosc7@gmail.com>
2025-09-07 14:39:39 -0600

    Create C_Estudiante.java

---
+++ Tareas Programacion II/Tarea 8/C_Estudiante.java
@@ -0,0 +1,20 @@
+package service;
+
+public class C_Estudiante {
+    private int carnet;
+
+    public C_Estudiante(){
+        this.carnet = carnet;
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+    public String toString(){
+        return "C_Estudiante" + carnet;
+    }
+}
