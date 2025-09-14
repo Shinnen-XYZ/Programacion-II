5ad1de39e77219c182533d17c65e409319bd80ee
Jeferson <jefersonmencosc7@gmail.com>
2025-09-14 12:09:15 -0600

    Create Perfil.java

---
+++ Tareas Programacion II/Tarea 9/Perfil.java
@@ -0,0 +1,46 @@
+package service;
+
+public class Perfil {
+    private String tipoPerfil;
+    private String descripcion;
+
+    public Perfil() {
+    }
+
+    public Perfil(String tipoPerfil, String descripcion) {
+        this.tipoPerfil = tipoPerfil;
+        this.descripcion = descripcion;
+    }
+
+    public String getTipoPerfil() {
+        return tipoPerfil;
+    }
+
+    public void setTipoPerfil(String tipoPerfil) {
+        this.tipoPerfil = tipoPerfil;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Perfil{" +
+                "tipoPerfil='" + tipoPerfil + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Perfil perfil = (Perfil) obj;
+        return toString().equals(perfil.toString());
+    }
+}
