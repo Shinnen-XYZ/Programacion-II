75e104359503f6e7ee1c73f33ac840f6e1aa5661
Jeferson <jefersonmencosc7@gmail.com>
2025-11-08 17:47:32 -0600

    Delete Tareas Programacion II/Tarea 14 directory

--- Tareas Programacion II/Tarea 14/ExcepcionTemperaturaInvalida.java
+++
@@ -1,7 +0,0 @@
-package excepcion;
-
-public class ExcepcionTemperaturaInvalida extends ExcepcionBebida {
-    public ExcepcionTemperaturaInvalida(String mensaje) {
-        super(mensaje);
-    }
-}
